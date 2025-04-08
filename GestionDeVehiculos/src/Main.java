4b20b730fcbc1d11d43665cf7b6a18bae41d86fe
valencora <coratolovalen@gmail.com>
2025-04-08 20:34:53 -0300

    arraylist vehiculos

--- GestionDeVehiculos/src/Main.java
+++ GestionDeVehiculos/src/Main.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Vehiculo vehiculo1 = new Vehiculo("ABC123", "Toyota", 2020, 1500);
-        Vehiculo camion1   = new Camion("JKL012", "Scania", 2018, 5000, true);
-        Vehiculo auto1     = new Auto("MNO345", "Honda", 2021, 800, 5);
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(new Vehiculo("ABC123", "Toyota", 2020, 1500));
+        vehiculos.add(new Camion("JKL012", "Scania", 2018, 5000, true));
+        vehiculos.add(new Auto("MNO345", "Honda", 2021, 800, 5));
 
-        VehiculoPrinter.imprimir(vehiculo1);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(camion1);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(auto1);
+        for (Vehiculo v : vehiculos) {
+            VehiculoPrinter.imprimir(v);
+            System.out.println("----------------");
+        }
     }
 }
