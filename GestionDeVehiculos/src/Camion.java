78dbfd787e6d30479638375c97852dc1b66611cc
valencora <coratolovalen@gmail.com>
2025-04-08 20:02:55 -0300

    crear subclase camion

---
+++ GestionDeVehiculos/src/Camion.java
@@ -0,0 +1,16 @@
+public class Camion extends Vehiculo {
+    private boolean tieneAcoplado;
+
+    public Camion(String patente, String marca, int anio, double capacidadCargaKg, boolean tieneAcoplado) {
+        super(patente, marca, anio, capacidadCargaKg);
+        this.tieneAcoplado = tieneAcoplado;
+    }
+
+    public boolean isTieneAcoplado() {
+        return tieneAcoplado;
+    }
+
+    public void setTieneAcoplado(boolean tieneAcoplado) {
+        this.tieneAcoplado = tieneAcoplado;
+    }
+}
