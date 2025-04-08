ac065a782e2c65035dc13401b43760fdb554c336
valencora <coratolovalen@gmail.com>
2025-04-08 20:09:48 -0300

    modificar vehiculo printer polimorfismo

--- GestionDeVehiculos/src/Main.java
+++ GestionDeVehiculos/src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Vehiculo vehiculo1 = new Vehiculo("ABC123", "Toyota", 2020, 1500);
-        Vehiculo vehiculo2 = new Vehiculo("DEF456", "Ford", 2019, 1800);
-        Vehiculo vehiculo3 = new Vehiculo("GHI789", "Chevrolet", 2021, 1300);
+        Camion camion1    = new Camion("JKL012", "Scania", 2018, 5000, true);
 
         VehiculoPrinter.imprimir(vehiculo1);
         System.out.println("----------------");
-        VehiculoPrinter.imprimir(vehiculo2);
-        System.out.println("----------------");
-        VehiculoPrinter.imprimir(vehiculo3);
+        VehiculoPrinter.imprimir(camion1);
     }
 }
