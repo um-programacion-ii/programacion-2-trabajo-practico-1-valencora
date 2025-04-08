66e9af6c8ac43f85440aabb0287cc56eec4aa1ea
valencora <coratolovalen@gmail.com>
2025-04-08 20:17:34 -0300

    crear clase auto

---
+++ GestionDeVehiculos/src/Auto.java
@@ -0,0 +1,22 @@
+public class Auto extends Vehiculo {
+    private int cantidadPasajeros;
+
+    public Auto(String patente, String marca, int anio, double capacidadCargaKg, int cantidadPasajeros) {
+        super(patente, marca, anio, capacidadCargaKg);
+        if (cantidadPasajeros <= 0) {
+            throw new IllegalArgumentException("La cantidad de pasajeros debe ser mayor a 0.");
+        }
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+
+    public int getCantidadPasajeros() {
+        return cantidadPasajeros;
+    }
+
+    public void setCantidadPasajeros(int cantidadPasajeros) {
+        if (cantidadPasajeros <= 0) {
+            throw new IllegalArgumentException("La cantidad de pasajeros debe ser mayor a 0.");
+        }
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+}
